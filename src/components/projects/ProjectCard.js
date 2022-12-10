2c99804bfec525e20a9af3c56ab811d8831d9acf
Robert Petersen <robert.petersen808@gmail.com>
2022-12-10 12:48:04 -0600

    created styles / rendered Projects / changed ProjectCard link logic

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -5,24 +5,24 @@ const ProjectCard = ({projectInfo}) => {
     <div className="projectCard_container">
       <div className="projectCard_content">
         <img className="projectCard_img" src={projectInfo.img} alt={projectInfo.alt} />
-        <h2>{projectInfo.title}</h2>
-        <p>{projectInfo.description}</p>
+        <h2 className="projectCard_title">{projectInfo.title}</h2>
+        <p className="projectCard_description">{projectInfo.description}</p>
       </div>
       <div className="links_conatiner">
         { 
-          projectInfo.isRepo //can change to check for empty string in link
-            ? <a className="repo_link" href={projectInfo.repoLink} target="_blank" rel="noreferrer">GitHub Repo</a> 
-            : ""
+          projectInfo.repoLink === "" 
+            ? "" 
+            : <a className="link" href={projectInfo.repoLink} target="_blank" rel="noreferrer">GitHub Repo</a>
         }
         { 
-          projectInfo.isDeploy 
-            ? <a className="deploy_link" href={projectInfo.deployLink} target="_blank" rel="noreferrer">Deployed Site</a> 
-            : ""
+          projectInfo.deployLink === "" 
+            ? ""
+            : <a className="link" href={projectInfo.deployLink} target="_blank" rel="noreferrer">Deployed Site</a>
         }
         { 
-          projectInfo.isBlog 
-            ? <a className="deploy_link" href={projectInfo.blogLink} target="_blank" rel="noreferrer">View Project Blog</a> 
-            : ""
+          projectInfo.blogLink === "" 
+            ? "" 
+            : <a className="link" href={projectInfo.blogLink} target="_blank" rel="noreferrer">View Project Blog</a>
         }
       </div>
     </div>
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -5,6 +5,7 @@ import MobileNav from "../components/mobileNav/MobileNav";
 import Landing from "../components/landing/Landing.js";
 import About from "../components/about/About";
 import Skills from "../components/skills/Skills";
+import Projects from "../components/projects/Projects";
 
 function HomeView() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,6 +21,7 @@ function HomeView() {
       <Landing />
       <About />
       <Skills />
+      <Projects />
     </section>
   );
 }
