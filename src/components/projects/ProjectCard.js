702c81b49a4fa8a30b0430ae35d05ddb16ca93f1
Robert Petersen <robert.petersen808@gmail.com>
2022-12-10 12:08:13 -0600

    Added project images to repo / completed projectData / completed ProjectCard / completed Projects

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,8 +1,31 @@
 import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = ({projectInfo}) => {
   return (
-    <div></div>
+    <div className="projectCard_container">
+      <div className="projectCard_content">
+        <img className="projectCard_img" src={projectInfo.img} alt={projectInfo.alt} />
+        <h2>{projectInfo.title}</h2>
+        <p>{projectInfo.description}</p>
+      </div>
+      <div className="links_conatiner">
+        { 
+          projectInfo.isRepo //can change to check for empty string in link
+            ? <a className="repo_link" href={projectInfo.repoLink} target="_blank" rel="noreferrer">GitHub Repo</a> 
+            : ""
+        }
+        { 
+          projectInfo.isDeploy 
+            ? <a className="deploy_link" href={projectInfo.deployLink} target="_blank" rel="noreferrer">Deployed Site</a> 
+            : ""
+        }
+        { 
+          projectInfo.isBlog 
+            ? <a className="deploy_link" href={projectInfo.blogLink} target="_blank" rel="noreferrer">View Project Blog</a> 
+            : ""
+        }
+      </div>
+    </div>
   );
 }
 
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,8 +1,19 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+import {projectData} from "./projectData";
 
 const Projects = () => {
   return (
-    <div></div>
+    <section id="projects">
+      <h1 className="projects_heading">What I've Created</h1>
+      <div className="projects_container">
+        {
+          projectData.map((projectInfo, index) => {
+            return <ProjectCard key={index} projectInfo={projectInfo}/>
+          })
+        }
+      </div>
+    </section>
   );
 }
 
