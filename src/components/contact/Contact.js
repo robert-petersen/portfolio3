21c5b97ab35e7bf27984cd7b6f43b932fb87fc0f
Robert Petersen <robert.petersen808@gmail.com>
2022-12-26 18:55:33 -0600

    Contact Responsiveness started

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -10,12 +10,8 @@ const Contact = () => {
         <div className="contact_content">
           <h1>Contact Me</h1>
           <p>
-            If you would like to get in touch with me about job opportunities or to work on a project together, feel free to do so by messaging me on <a href="https://www.linkedin.com/in/robert-petersen808/" target="_blank" rel="noreferrer">LinkedIn</a> or by sending an email to my business email: <span>robert.petersen808@gmail.com</span>.
+            If you would like to get in touch with me about job opportunities or to work on a project together, feel free to do so by messaging me on <a href="https://www.linkedin.com/in/robert-petersen808/" target="_blank" rel="noreferrer">LinkedIn</a> or by sending an email to my business email: <span>robert.petersen808@gmail.com</span>. Check out my social media and projects I've been working on below!
           </p>
-        </div>
-        <div className="contact_content">
-          <h1>Check Out More</h1>
-          <p>Check out my social media and projects I've been working on here!</p>
           <div className="contact_socials_container">
             {
               socialData.map((socialInfo, index) => {
